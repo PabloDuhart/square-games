034d7dc4172893bdbfbdce61631e8fcd55e048b3
Quimoly <pablo.duhart@alumnos.ucn.cl>
2020-04-30 18:21:52 -0400

    Testeando scripts

---
+++ Assets/Scripts/projectile_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile_Script : MonoBehaviour
+{
+
+	private bool aiming = false;
+
+	private Rigidbody2D rb;
+    
+    void Start()
+    {
+       
+    }
+
+	private
+   
+    void Update()
+    {
+        
+    }
+}
