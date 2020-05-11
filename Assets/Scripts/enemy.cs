ee7e734c2d961984496424ee6a4dde4eb8fe4407
Quimoly <pablo.duhart@alumnos.ucn.cl>
2020-05-11 15:28:30 -0400

    Scrits projectile and enemy

---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+	public float health = 2f;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if( collision.relativeVelocity.magnitude > health)
+		{
+			Die();
+		}
+	}
+
+	void Die()
+	{
+		//Here you've to animate the death of the enemy
+		Destroy(gameObject);
+	}
+}
