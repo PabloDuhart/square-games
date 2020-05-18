35fd241546be1d90f69fc8d163cf7f39b30b6155
Quimoly <pablo.duhart@alumnos.ucn.cl>
2020-05-18 16:14:39 -0400

    UI Update

---
+++ Assets/main_menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_menu : MonoBehaviour
+{
+
+	public void GameSelecgLoad()
+	{
+
+	}
+
+	public void OptionsLoad()
+	{
+
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
