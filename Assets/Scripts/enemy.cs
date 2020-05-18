3775341921edef44d6ba46bdc4200eaec75d0151
reunion7 <51376566+reunion7@users.noreply.github.com>
2020-05-18 15:56:16 -0400

    Game 1 Mechanics

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
 	public float health = 2f;
+    public float enemyContact = 2f;
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if( collision.relativeVelocity.magnitude > health)
 		{
-			Die();
+            enemyContact--;
+            if (enemyContact<=0)
+            {
+                Die();
+            }
+            
 		}
+
 	}
 	void Die()
 	{
