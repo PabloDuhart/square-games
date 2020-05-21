fc1eafe8b5f188a738c6546c960683efefc7a0b9
JhonatanAl <jhonatan.alvarez@alumnos.ucn.cl>
2020-05-21 00:23:26 -0400

    SpaceShip Animations
    SpaceShip states controlled by code
    Enemy states controlled by code
    ** The interactions betwen SpaceShip and Projectil(2) aren't defined

--- Assets/Scripts/SpaceShip_Controller.cs
+++ Assets/Scripts/SpaceShip_Controller.cs
@@ -10,8 +10,12 @@ public class SpaceShip_Controller : MonoBehaviour
     public Vector2 ground;
     public float startDelay;
     private float distance;
+    public float velocity;
     private bool preLand;
+    private bool landing;
     public Vector3 spaceShipScale;
+    private float groundPositionY;
+    private bool toSpace;
     void Start()
     {
         anim = GetComponent<Animator>();
@@ -19,44 +23,69 @@ public class SpaceShip_Controller : MonoBehaviour
         gameObject.transform.rotation = new Quaternion(0f, 0f, -45, 110);//Change rotation of spaceShip
         gameObject.transform.localPosition = start;//Change position of spaceShip
         anim.SetBool("Grounded", false);
+        anim.SetBool("FirstReload", false);
+        anim.SetBool("Idle", false);
         preLand = false;
         distance = 0;
         anim.SetBool("Flying", true);
+        toSpace = true;
+        groundPositionY = ground.y;
     }
 
     // Update is called once per frame
     void Update()
-    {   
-        if (gameObject.transform.localPosition.Equals(preLanding))
+    {
+        Debug.Log((Mathf.Abs(gameObject.transform.position.y - groundPositionY)));
+        if (toSpace)
         {
-            preLand = true;
-            gameObject.transform.localScale = spaceShipScale;
-            gameObject.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
-            distance = 0.01f;
-        }
-        else
-        {
-            gameObject.transform.localPosition = Vector2.MoveTowards(start, preLanding, distance);
-            distance += 0.01f;
+            if (gameObject.transform.localPosition.Equals(preLanding))
+            {
+                preLand = true;
+                gameObject.transform.localScale = spaceShipScale;
+                gameObject.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+                distance = velocity;
+                toSpace = false;
+            }
+            else
+            {
+                gameObject.transform.localPosition = Vector2.MoveTowards(start, preLanding, distance);
+                distance += velocity;
+            }
         }
         if (preLand)
         {
             gameObject.transform.localPosition = Vector2.MoveTowards(preLanding, ground, distance);
-            distance += 0.01f;
+            distance += velocity;
+        }
+        if ((Mathf.Abs(gameObject.transform.position.y-groundPositionY))<1f & (Mathf.Abs(gameObject.transform.position.y - groundPositionY))!=0 & !landing)
+        {//if the distance between the spaceship and the ground is less than 1 and !=0 the object is landing
+            preLand = false;
+            landing = true;
+            anim.SetBool("Flying", false);
+            gameObject.transform.localPosition = Vector2.MoveTowards(preLanding, ground, distance);
+            distance += velocity;
+            StartCoroutine(WaitLanding());
+        }
+        if (landing)
+        {
+            gameObject.transform.localPosition = Vector2.MoveTowards(preLanding, ground, distance);
+            distance += velocity;
+        }
+        if ((Mathf.Abs(gameObject.transform.position.y - groundPositionY)) == 0)
+        {
+            //anim.SetBool("Grounded", true);
+            StartCoroutine(Wait());
         }
         
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    IEnumerator WaitLanding()
     {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            Debug.Log("entra al collision");
-            preLand = false;
-            gameObject.transform.localPosition = Vector2.MoveTowards(preLanding, ground, distance);
-            distance += 0.01f;
-            anim.SetBool("Flying", false);
-           
-            anim.SetBool("Grounded", true);
-        }
+        yield return new WaitForSeconds(1.2f);
+        anim.SetBool("Grounded", true);
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(6.05f);
+        anim.SetBool("FirstReload", true);
     }
 }
