36cee8d49040175b14b22138a79b72e0e6e3c9d3
Quimoly <pablo.duhart@alumnos.ucn.cl>
2020-05-30 12:12:48 -0400

    structure spript uptade

--- Assets/structure_script.cs
+++ Assets/structure_script.cs
@@ -8,30 +8,50 @@ public class structure_script : MonoBehaviour
 
 	public int forceToHit;
 
+	public Sprite State1;
+	public Sprite State2;
+	public Sprite State3;
 	private int counter = 0;
+	
 
 
-    
-    void Update()
+	void Update()
     {
+		SpriteRenderer[] childrenSprites = GetComponentsInChildren<SpriteRenderer>();
+
 		if (maxHits <= counter)
 		{
 			Destroy(gameObject);
 		}
 		if (counter == 1)
 		{
+			for (int i = 0; i < childrenSprites.Length; i++)
+			{
+				childrenSprites[i].sprite = State1;
+			}
 			//asignar sprite un hit
 		}
 		if (counter == 2)
 		{
+			for (int i = 0; i < childrenSprites.Length; i++)
+			{
+				childrenSprites[i].sprite = State2;
+			}
 			//asignar sprite dos hit
 		}
 		if (counter == 3)
 		{
+			for (int i = 0; i < childrenSprites.Length; i++)
+			{
+				childrenSprites[i].sprite = State3;
+			}
 			//asignar sprite tres hit
 		}
 	
 	}
+
+
+
 
 	private void OnCollisionEnter2D(Collision2D collision)
 	{
@@ -43,9 +63,4 @@ public class structure_script : MonoBehaviour
 			}
 		}
 	}
-
-
-
-
-
 }
