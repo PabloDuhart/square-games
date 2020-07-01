d45ff2ce32c0b9e2ece88bca650949937aab73e9
PaNZeRHiP <64170998+PaNZeRHiP@users.noreply.github.com>
2020-07-01 17:48:56 -0400

    Game 2 Gravity

---
+++ Assets/GravityGame2.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGame2 : MonoBehaviour
+{
+    [Header("Gratity Spawn Times")]
+    //private int rotationSpeed;
+    public int gravitationRadius;
+    public int gravitation;
+
+
+    //private Color childrenColor;
+
+    private bool gravityI;
+
+    private CircleCollider2D gravitationTrigger;
+
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        foreach (var objectInVicinity in objectsInRange)
+        {
+            if (objectInVicinity == null)
+            {
+                objectsInRange.Remove(objectInVicinity);
+                break;
+            }
+
+            objectInVicinity.gravityScale = 0f;
+            float dist = Vector2.Distance(transform.position, objectInVicinity.transform.position);
+            float gravitationFactor = -1 + dist; //for attraction change -1 to 1
+            Vector2 force = (transform.position - objectInVicinity.transform.position).normalized * gravitation * gravitationFactor;
+            objectInVicinity.AddForce(force);
+        }
+
+        
+
+          gravitationTrigger = GetComponent<CircleCollider2D>();
+          gravitationTrigger.isTrigger = true;
+          gravitationTrigger.radius = gravitationRadius / transform.localScale.x;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, gravitationRadius);
+    }
+
+    private List<Rigidbody2D> objectsInRange = new List<Rigidbody2D>();
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var rb = collider.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            objectsInRange.Add(rb);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var rb = collider.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.gravityScale = 1f;
+            objectsInRange.Remove(rb);
+            
+        }
+    }
+
+    
+}
