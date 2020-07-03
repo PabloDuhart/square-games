1d39ddecf7566b0f70617b4575177722bbaea10c
PaNZeRHiP <64170998+PaNZeRHiP@users.noreply.github.com>
2020-07-02 21:04:25 -0400

    Game 2 UPDATE Gravitation
    Dou Trabajo

---
+++ Assets/GravityRec.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityRec : MonoBehaviour
+{
+    public int impulseX;
+    public int impulseY;
+    
+
+    private SpriteRenderer spriterender;
+    private bool gravityI;
+    private BoxCollider2D gravitationTrigger;
+
+
+    void FixedUpdate()
+    {
+
+        foreach (var objectInVicinity in objectsInRange)
+        {
+            if (objectInVicinity == null)
+            {
+                objectsInRange.Remove(objectInVicinity);
+                break;
+            }
+
+            objectInVicinity.gravityScale = 0f;
+            
+            objectInVicinity.AddForce(new Vector2(impulseX,impulseY));
+        }
+
+
+        gravitationTrigger = GetComponent<BoxCollider2D>();
+        gravitationTrigger.isTrigger = true;
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position,gravitationTrigger.size);
+    }
+
+    private List<Rigidbody2D> objectsInRange = new List<Rigidbody2D>();
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        var rb = collider.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            objectsInRange.Add(rb);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        var rb = collider.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.gravityScale = 1f;
+            objectsInRange.Remove(rb);
+
+        }
+    }
+}
