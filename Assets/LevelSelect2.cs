d5f2d9c7bcfe574fee764c71c0761c5698dd865c
Quimoly <pablo.duhart@alumnos.ucn.cl>
2020-08-17 12:59:41 -0400

    Update

---
+++ Assets/LevelSelect2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelSelect2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Button[] levelButtons;
+
+
+
+    void Start()
+    {
+
+        int levelReached = PlayerPrefs.GetInt("levelReached2", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+            else
+            {
+                levelButtons[i].interactable = true;
+            }
+        }
+        PlayerPrefs.Save();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached2", 1);
+
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+            else
+            {
+                levelButtons[i].interactable = true;
+            }
+        }
+        PlayerPrefs.Save();
+    }
+}
--- Assets/Scripts/main_menu.cs
+++ Assets/Scripts/main_menu.cs
@@ -25,15 +25,29 @@ public class main_menu : MonoBehaviour
 	}
 
 
-	public void LoadGame1()
+
+	public void LoadGame1Lvl1()
 	{
-		SceneManager.LoadScene("Map");
+		SceneManager.LoadScene("G1Nivel1");
 	}
 
-	public void Game2Lvl1()
+	public void LoadGame1Lvl2()
 	{
-		SceneManager.LoadScene("Nivel1G2");
+		SceneManager.LoadScene("G1Nivel2");
 	}
+	public void LoadGame1Lvl3()
+	{
+		SceneManager.LoadScene("G1Nivel3");
+	}
+	public void LoadGame1Lvl4()
+	{
+		SceneManager.LoadScene("G1Nivel4");
+	}
+	public void LoadGame1Lvl5()
+	{
+		SceneManager.LoadScene("G1Nivel5");
+	}
+
 
 	public void Game2Lvl2()
 	{
@@ -58,6 +72,7 @@ public class main_menu : MonoBehaviour
 	public void RestartStats()
 	{
 		PlayerPrefs.SetInt("levelReached", 1);
+		PlayerPrefs.SetInt("levelReached2", 1);
 		PlayerPrefs.Save();
 	}
 
